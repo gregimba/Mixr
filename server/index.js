161f1f1b0d4c11a913e985167e530fd78af71a7b
ALB24 <universidad.aless@gmail.com>
2018-04-10 11:48:43 -0500

    setup initial routings

--- server/index.js
+++ server/index.js
@@ -1,8 +1,20 @@
-const express = require('express');
-const path = require('path');
+const express = require("express");
+const path = require("path");
 
 const app = express();
 
-app.use('/', express.static(path.join(__dirname, '../client/dist')));
+app.use("/", express.static(path.join(__dirname, "../client/dist")));
 
-app.listen(3000, () => console.log('Example app listening on port 3000!'));
+// Obj. of single drink
+app.get("/drinks/:id", (req, res) => {});
+
+// Array of drink matches
+app.get("/user/:id/drinks", (req, res) => {});
+
+// Adding user ingrindents to the DB
+app.post("/user/:id/ingredients/:id", (req, res) => {});
+
+// Array of ingredients
+app.get("/user/:id/ingredients", (req, res) => {});
+
+app.listen(3000, () => console.log("Example app listening on port 3000!"));
