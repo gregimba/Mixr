dd4fd78e36fc2088fc65823b5b02db5d494b1006
Grant Regimbal <regimbal.grant@gmail.com>
2018-04-10 17:10:39 -0500

    dotenv example

--- server/database/index.js
+++ server/database/index.js
@@ -1,1 +1,30 @@
-// write the database here
+require('dotenv').config();
+let Sequelize = require('sequelize');
+
+console.log(process.env.DB_NAME);
+const db = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USERNAME,
+  process.env.DB_PASSWORD,
+  {
+    host: 'localhost',
+    dialect: 'postgres',
+    operatorsAliases: false,
+
+    pool: {
+      max: 5,
+      min: 0,
+      acquire: 30000,
+      idle: 10000
+    }
+  }
+);
+
+db
+  .authenticate()
+  .then(() => {
+    console.log('Connection has been established successfully.');
+  })
+  .catch(err => {
+    console.error('Unable to connect to the database:', err);
+  });
