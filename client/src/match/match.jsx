1bd91fa3938b5593c3c95276ae581b465bcd9e48
Jake Hsiao <jakehsiao@Jakes-MacBook-Pro.local>
2018-04-11 16:21:53 -0500

    delete unnecessary line

--- client/src/match/match.jsx
+++ client/src/match/match.jsx
@@ -6,15 +6,10 @@ const Match = (props) => (
     <div className="drink-images">
       <img className="drink-object" />
     </div> 
-    <div className="drink-body">
       <div className="drink-title"></div>
-      <div className="drink-instruction"></div>
-      <div className="drink-glass"></div>
-    </div> 
   </div>
 
 )
 
 
 export default Match;
-
