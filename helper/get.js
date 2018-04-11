b925f71ef9b3ab83294a044d9b9db0317d1cf6fc
Grant Regimbal <regimbal.grant@gmail.com>
2018-04-11 17:22:57 -0500

    inital db writes

--- helper/get.js
+++ helper/get.js
@@ -1,5 +1,6 @@
 const fs = require('fs');
 const request = require('request');
+const db = require('../server/database/models');
 let data = fs.readFileSync('drinks.json');
 
 const drink_url = 'https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=';
@@ -27,15 +28,28 @@ function writeFile(err, res, body) {
       request.get(drink_url + drink.idDrink, (err, res, body) => {
         if (err) {
         } else if (res.statusCode === 200) {
-          console.log(JSON.parse(body));
-          wait(100);
+          let drinkData = JSON.parse(body).drinks[0];
+
+          console.log(drinkData.strDrink);
+
+          db.Drink.create({
+            name: drinkData.strDrink,
+            glass: drinkData.strGlass,
+            instruction: drinkData.strInstructions,
+            image: drinkData.strDrinkThumb
+          });
+
+          wait(500);
         }
       });
     }
   }
 }
 
-for (let glass of glasses) {
-  let url = 'https://www.thecocktaildb.com/api/json/v1/1/filter.php?g=' + glass;
-  request.get(url, writeFile);
-}
+db.sequelize.sync().then(() => {
+  for (let glass of glasses) {
+    let url =
+      'https://www.thecocktaildb.com/api/json/v1/1/filter.php?g=' + glass;
+    request.get(url, writeFile);
+  }
+});
