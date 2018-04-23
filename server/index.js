47729f16dfcbaf5415e50cad3c244a863fa32b9c
eerilai <eerilaie@gmail.com>
2018-04-23 16:50:49 -0500

    Clean server/index.js

--- server/index.js
+++ server/index.js
@@ -1,4 +1,4 @@
-if (process.env.NODE_ENV != 'production') {
+if (process.env.NODE_ENV !== 'production') {
   require('dotenv').config();
 }
 const express = require('express');
@@ -28,13 +28,11 @@ app.use('/', express.static(path.join(__dirname, '../client/dist')));
 app.set('view engine', 'ejs');
 
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(
-  require('express-session')({
-    secret: 'process.env.SESSION_SECRET',
-    resave: false,
-    saveUninitialized: false
-  })
-);
+app.use(require('express-session')({
+  secret: 'process.env.SESSION_SECRET',
+  resave: false,
+  saveUninitialized: false,
+}));
 
 app.use(passport.initialize());
 app.use(passport.session());
@@ -43,43 +41,50 @@ passport.use(db.users.createStrategy());
 passport.serializeUser(db.users.serializeUser());
 passport.deserializeUser(db.users.deserializeUser());
 
-app.get('/', isLoggedIn, function(req, res) {
+const isLoggedIn = (req, res, next) => {
+  if (req.isAuthenticated()) {
+    return next();
+  }
+  res.redirect('/home');
+};
+
+app.get('/', isLoggedIn, (req, res) => {
   res.render('');
 });
 
-app.get('/home', function(req, res) {
+app.get('/home', (req, res) => {
   res.render('home');
 });
 
-app.get('/secret', function(req, res) {
+app.get('/secret', (req, res) => {
   res.render('secret');
 });
 
-app.get('/accountCreated', isLoggedIn, function(req, res) {
+app.get('/accountCreated', isLoggedIn, (req, res) => {
   res.render('accountCreated');
 });
 
-app.get('/register', function(req, res) {
+app.get('/register', (req, res) => {
   res.render('register');
 });
 
-app.post('/register', function(req, res) {
+app.post('/register', (req, res) => {
   db.users.register(
     new db.users({ username: req.body.username }),
     req.body.password,
-    function(err, user) {
+    (err, user) => {
       if (err) {
         console.log(err);
         return res.redirect('/register');
       }
-      passport.authenticate('local')(req, res, function() {
+      passport.authenticate('local')(req, res, () => {
         res.redirect('/accountCreated');
       });
-    }
+    },
   );
 });
 
-app.get('/login', function(req, res) {
+app.get('/login', (req, res) => {
   res.render('login');
 });
 
@@ -87,38 +92,28 @@ app.post(
   '/login',
   passport.authenticate('local', {
     successRedirect: '/',
-    failureRedirect: '/secret'
+    failureRedirect: '/secret',
   }),
-  function(req, res) {}
+  (req, res) => {},
 );
 
-app.get('/logout', function(req, res) {
+app.get('/logout', (req, res) => {
   req.logout();
   res.redirect('/home');
 });
 
-function isLoggedIn(req, res, next) {
-  if (req.isAuthenticated()) {
-    return next();
-  }
-  res.redirect('/home');
-}
-
 // Checking on session to retrieve userId
-app.get('/session', isLoggedIn, function(req, res) {
-  db.users
-    .find({
-      where: {
-        username: req.session.passport.user
-      }
-    })
-    .then(user => {
-      res.send(user);
-    })
-    .catch(err => {
-      console.error('!!Err: ', err);
-      res.sendStatus(404);
-    });
+app.get('/session', isLoggedIn, (req, res) => {
+  db.users.find({
+    where: {
+      username: req.session.passport.user,
+    },
+  }).then((user) => {
+    res.send(user);
+  }).catch((err) => {
+    console.error('!!Err: ', err);
+    res.sendStatus(404);
+  });
 });
 
 // Adding user "liked" ingrindents to the DB
@@ -127,19 +122,16 @@ app.post('/user/:userId/ingredients/:ingredientId', (req, res) => {
   let ingredientID = req.params.ingredientId;
 
   // Add ingredient to user in interjoin table
-  db.user_ingredient
-    .findOrCreate({
-      where: {
-        userId: userID,
-        ingredientId: ingredientID
-      }
-    })
-    .then(async () => {
-      console.log('****You Created A User!!!');
-      const newMatches = await matchUserWithDrinks(userID);
-      res.json(newMatches);
-    })
-    .catch('Error Posting to DB');
+  db.user_ingredient.findOrCreate({
+    where: {
+      userId: userID,
+      ingredientId: ingredientID,
+    },
+  }).then(async () => {
+    console.log('****You Created A User!!!');
+    const newMatches = await matchUserWithDrinks(userID);
+    res.json(newMatches);
+  }).catch('Error Posting to DB');
 });
 
 // Will be quering a drinkId and send back the object of
@@ -147,173 +139,122 @@ app.post('/user/:userId/ingredients/:ingredientId', (req, res) => {
 app.get('/drink/:drinkId', (req, res) => {
   let drinkID = req.params.drinkId;
 
-  db.drink
-    .find({
-      where: { id: drinkID }
-    })
-    .then(singleDrink => {
-      db.drinkingredient
-        .findAll({
-          where: { drinkId: drinkID },
-          include: [
-            {
-              model: db.ingredient
-            }
-          ]
-        })
-        .then(singleDrinkIngredients => {
-          let drinkIngredients = {};
+  db.drink.find({
+    where: { id: drinkID },
+  }).then((singleDrink) => {
+    db.drinkingredient
+      .findAll({
+        where: { drinkId: drinkID },
+        include: [
+          {
+            model: db.ingredient,
+          },
+        ],
+      })
+      .then((singleDrinkIngredients) => {
+        const drinkIngredients = {};
 
-          singleDrinkIngredients.forEach(drinkIngredient => {
-            let ingredientName = drinkIngredient.ingredient.dataValues.name;
-            let ingredientMeasure = drinkIngredient.dataValues.measure;
-            console.log(
-              '****Drink IDs',
-              drinkIngredient.ingredient.dataValues.id
-            );
-
-            drinkIngredients[ingredientName] = ingredientMeasure;
-          });
-
-          let strID = singleDrink.dataValues.strId;
-
-          singleDrink.dataValues.ingredients = drinkIngredients;
-          singleDrink.dataValues.image = `http://assets.absolutdrinks.com/drinks/400x400/${strID}.png`;
-
-          singleDrink = singleDrink.dataValues;
-
-          res.json(singleDrink);
-        })
-        .catch(err => {
-          console.log('Error:', err);
+        singleDrinkIngredients.forEach((drinkIngredient) => {
+          const ingredientName = drinkIngredient.ingredient.dataValues.name;
+          const ingredientMeasure = drinkIngredient.dataValues.measure;
+          console.log('****Drink IDs', drinkIngredient.ingredient.dataValues.id);
+          drinkIngredients[ingredientName] = ingredientMeasure;
         });
-    })
-    .catch(err => {
-      console.log('Error:', err);
-    });
+
+        const strID = singleDrink.dataValues.strId;
+        singleDrink.dataValues.ingredients = drinkIngredients;
+        singleDrink.dataValues.image = `http://assets.absolutdrinks.com/drinks/400x400/${strID}.png`;
+        singleDrink = singleDrink.dataValues;
+
+        res.json(singleDrink);
+      }).catch((err) => {
+        console.log('Error:', err);
+      });
+  }).catch((err) => {
+    console.log('Error:', err);
+  });
 });
 
 // Array of drink matches for a user
 app.get('/user/:userId/drinks', (req, res) => {
-  let userID = req.params.userId;
-  db.users
-    .findAll({
-      where: { id: userID },
-      include: [
-        {
-          model: db.drink
-        }
-      ]
-    })
-    .then(user => {
-      let userDrinkList = [];
-      user[0].drinks.forEach(userDrink => {
-        userDrink = userDrink.dataValues;
-        let strID = userDrink.strId;
-        userDrink.image = `http://assets.absolutdrinks.com/drinks/400x400/${strID}.png`;
-        userDrinkList.unshift(userDrink);
-      });
-
-
-      res.json(userDrinkList);
-    })
-    .catch(err => {
-      console.log('!!!Error:', err);
+  const userID = req.params.userId;
+  db.users.findAll({
+    where: { id: userID },
+    include: [{
+      model: db.drink,
+    }],
+  }).then((user) => {
+    const userDrinkList = [];
+    user[0].drinks.forEach((userDrink) => {
+      userDrink = userDrink.dataValues;
+      const strID = userDrink.strId;
+      userDrink.image = `http://assets.absolutdrinks.com/drinks/400x400/${strID}.png`;
+      userDrinkList.unshift(userDrink);
     });
-});
-
-// Array of ingredients matches for user, returns array of all 'liked' ingredients
-app.get('/user/:userId/ingredients', (req, res) => {
-  let userID = req.params.userId;
-  db.users
-    .findAll({
-      where: { id: userID },
-      include: [
-        {
-          model: db.ingredient
-        }
-      ]
-    })
-    .then(user => {
-      let likedIngredientList = [];
-      user[0].ingredients.forEach(userIngredient => {
-        userIngredient.dataValues.image = 'Some Image';
-        likedIngredientList.push(userIngredient.dataValues);
-      });
-      res.json(likedIngredientList);
-    })
-
-    .catch(err => {
-      console.log('!!!Error:', err);
-    });
+    res.json(userDrinkList);
+  }).catch((err) => {
+    console.log('!!!Error:', err);
+  });
 });
 
 // Returns a Single "non-liked" ingredient
 // (reverse of "liked" ingredients list and then send back a single random 'non-liked' ingredient)
 app.get('/user/:userId/randomIngredient', (req, res) => {
-  let userID = req.params.userId;
+  const userID = req.params.userId;
 
-  db.ingredient
-    .findAll({})
-    .then(ingredients => {
-      db.users
-        .findAll({
-          where: { id: userID },
-          include: [
-            {
-              model: db.ingredient
-            }
-          ]
-        })
-        // .then(async user => {
-        .then( user => {
-          // Creates a list of all the ingrdients in the database
-          let listOfAllIngredients = [];
-          ingredients.forEach(allIngredients => {
-            listOfAllIngredients.push(allIngredients.dataValues);
-          });
+  db.ingredient.findAll({
+  }).then((ingredients) => {
+    db.users.findAll({
+      where: { id: userID },
+      include: [{
+        model: db.ingredient,
+      }],
+    }).then((user) => {
+      // Creates a list of all the ingrdients in the database
+      const listOfAllIngredients = [];
+      ingredients.forEach((allIngredients) => {
+        listOfAllIngredients.push(allIngredients.dataValues);
+      });
 
-          // Creates a list of all the 'liked' ingredients for a user
-          let likedIngredientList = [];
-          user[0].ingredients.forEach(userIngredient => {
-            likedIngredientList.push(userIngredient.dataValues);
-          });
+      // Creates a list of all the 'liked' ingredients for a user
+      const likedIngredientList = [];
+      user[0].ingredients.forEach((userIngredient) => {
+        likedIngredientList.push(userIngredient.dataValues);
+      });
 
-          // Creates a list of 'non-liked' ingredients
-          let notLikedIngredientList = [];
+      // Creates a list of 'non-liked' ingredients
+      const notLikedIngredientList = [];
 
-          listOfAllIngredients.forEach(allIngredient => {
-            let notLikedCheck = likedIngredientList.filter(
-              ingredient => ingredient.strID === allIngredient.strID
-            );
-
-            if (notLikedCheck.length === 0) {
-              notLikedIngredientList.push(allIngredient);
-            }
-          });
-          // Selects a random index in the 'non-liked' list and
-          // sends back an object with one random 'non-liked' ingredient
-          let randomIngredient;
-          if (notLikedIngredientList.length > 0) {
-            randomIngredient =
-              notLikedIngredientList[
-                Math.floor(Math.random() * notLikedIngredientList.length)
-              ];
-            // try {
-            //   randomIngredient.image = await searchImage(randomIngredient.name);
-            //   randomIngredient.image = randomIngredient.image[0].url;
-            // } catch (err) {
-            //   console.error('ERROR RETRIEVING IMAGE: ', err);
-            // }
-          } else {
-            randomIngredient = null;
-          }
-          res.json(randomIngredient);
-        });
-    })
-    .catch(err => {
-      console.log('!!!Error:', err);
+      listOfAllIngredients.forEach((allIngredient) => {
+        const notLikedCheck = likedIngredientList
+          .filter(ingredient => ingredient.strID === allIngredient.strID);
+        if (notLikedCheck.length === 0) {
+          notLikedIngredientList.push(allIngredient);
+        }
+      });
+      // Selects a random index in the 'non-liked' list and
+      // sends back an object with one random 'non-liked' ingredient
+      let randomIngredient;
+      if (notLikedIngredientList.length > 0) {
+        randomIngredient =
+          notLikedIngredientList[
+            Math.floor(Math.random() * notLikedIngredientList.length)
+          ];
+        // retrieve first image from google images
+        // try {
+        //   randomIngredient.image = await searchImage(randomIngredient.name);
+        //   randomIngredient.image = randomIngredient.image[0].url;
+        // } catch (err) {
+        //   console.error('ERROR RETRIEVING IMAGE: ', err);
+        // }
+      } else {
+        randomIngredient = null;
+      }
+      res.json(randomIngredient);
     });
+  }).catch((err) => {
+    console.log('!!!Error:', err);
+  });
 });
 
 // sequelize.sync().then(() => {
