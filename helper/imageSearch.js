bdcfbd1a0c0747de359cf199245755fb40034df8
eerilai <eerilaie@gmail.com>
2018-04-23 16:51:49 -0500

    Remove size restriction in imageSearch for better results

--- helper/imageSearch.js
+++ helper/imageSearch.js
@@ -4,7 +4,7 @@ const searchImages = query =>
   new Promise((res, rej) => {
     const images = new ImageSearch(process.env.CSE_ID, process.env.CSE_API_KEY);
     images
-      .search(query, { size: 'medium', safe: 'high' })
+      .search(query, { safe: 'high' })
       .then(imgs => res(imgs))
       .catch(err => rej(err));
   });
