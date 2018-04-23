59619484884ae0a59497d137c2402a7cccf520ac
Grant Regimbal <regimbal.grant@gmail.com>
2018-04-23 02:00:25 -0500

    inital refactor to classes

--- client/src/drink/drink.jsx
+++ client/src/drink/drink.jsx
@@ -1,19 +1,30 @@
 import React from 'react';
 import './Drink.css';
 
-const Drink = props => (
-  <div className="drink-page">
-    <div className="drink-image">
-      {/* <img className="drink-object" src={'drink image url goes here...'} /> */}
-    </div>
-    <div className="drink-title">{props.drink.name}</div>
-    <div className="drink-instruction">{props.drink.instruction}</div>
-    <div className="drink-ingredients">{'drink ingredient goes here...'}</div>
-    <div className="drink-glass">{props.drink.glass}</div>
-    <span className="exit-button" onClick={props.handleClick}>
-      &times;
-    </span>
-  </div>
-);
+class Drink extends React.Component {
+  render() {
+    return (
+      <div className="drink-page">
+        <div className="drink-image">
+          <img
+            className="drink-object"
+            src={`http://assets.absolutdrinks.com/drinks/400x400/${
+              this.props.drink.strId
+            }.png`}
+          />
+        </div>
+        <div className="drink-title">{this.props.drink.name}</div>
+        <div className="drink-instruction">{this.props.drink.instruction}</div>
+        <div className="drink-ingredients">
+          {'drink ingredient goes here...'}
+        </div>
+        <div className="drink-glass">{this.props.drink.glass}</div>
+        <span className="exit-button" onClick={this.props.handleClick}>
+          &times;
+        </span>
+      </div>
+    );
+  }
+}
 
 export default Drink;
